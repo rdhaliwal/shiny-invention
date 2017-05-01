f6dbcd4d217d27a3d887c59a283fccc310140ec6
Randeep Dhaliwal <dhaliwal.randeep.s@gmail.com>
2017-05-01 17:57:18 +1000

    Adding Redux Actions and ActionCreators
    Actions are similar to Actions in Flux, but slightly different. An
    Action is simply a JSON object of information that tells the state
    how it should be modified. In this commit, one example of an action is:
    
    { type: ENROL_STUDENT, name: name }
    
    Actions always have a type that instruct a Reducer how to modify the
    state, as well as any additional information that the Reducer may need
    to complete this action. Reducers are the actual function that gets
    executed to modify the state (this will be covered in the next commit or
    two in greater detail).
    
    However, we need to execute some kind of function to actually send the
    Action to the Reducer, which is what we call ActionCreators. Following
    on from the above example, we use the following action creator:
    
    export const enrolStudent (name) => {
        let action = { type: ENROL_STUDENT, name: name }
        if (name !== null) { return action; }
    }
    
    This is a simple enough function that can do some business logic-y type
    things, that when done, returns the action. There's some middleware that
    Redux will cover that connects this ActionCreator to the Reducer, which
    again, we'll cover in the next commit or two.

---
+++ src/app/actions/StudentActions.js
@@ -0,0 +1,19 @@
+/*
+ * Action types
+ */
+
+export const ENROL_STUDENT =  'ENROL_STUDENT'
+export const EXPELL_STUDENT = 'EXPELL_STUDENT'
+
+/*
+ * Action creators
+ */
+
+export const enrolStudent (name) => {
+  let action = { type: ENROL_STUDENT, name: name }
+  if (name !== null) { return action; }
+}
+
+export const expellStudent (name) => {
+  return { type: EXPELL_STUDENT, name: name }
+}
---
+++ src/app/actions/TeacherActions.js
@@ -0,0 +1,18 @@
+/*
+ * Action types
+ */
+
+export const HIRE_TEACHER = 'HIRE_TEACHER'
+export const FIRE_TEACHER = 'FIRE_TEACHER'
+
+/*
+ * Action creators
+ */
+
+export const hireTeacher (name) => {
+  return { type: HIRE_TEACHER, name: name }
+}
+
+export const fireTeacher (name) => {
+  return { type: FIRE_TEACHER, name: name }
+}
